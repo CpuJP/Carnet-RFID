b89e570b1c2fc474b27ccfd8124231822676f5d4
CpuJP <cpujuanpis@gmail.com>
2022-04-04 20:46:20 -0500

    error fixing in tables with indexed data, added query data for data entry

--- carnet.sql
+++ carnet.sql
@@ -37,7 +37,7 @@ nariz bit not null,
 garganta bit not null,
 olores bit not null,
 diarrea bit not null,
-temperatura bit,
+temperatura decimal,
 ingresar_elementos bit not null,
 bicicleta bit,
 carro bit,
@@ -55,8 +55,10 @@ carrera ENUM('Admin Empresas', 'Contaduría Pública', 'Gestión Turística', 'A
 sede ENUM('Fusagasugá', 'Facatativá', 'Girardot', 'Soacha', 'Ubaté', 'Zipaquirá', 'Chía', 'Bogotá') not null,
 semestre int not null,
 id_persona_estudiante varchar(15) not null,
+unique key unique_id_persona_estudiante(id_persona_estudiante),
 foreign key (id_persona_estudiante) references personal_universidad(id_persona),
 id_rfid_nfc_estudiante varchar(50) not null,
+unique key unique_id_rfid_nfc_estudiante(id_rfic_nfc_estudiante),
 foreign key (id_rfid_nfc_estudiante) references rfid_nfc(id_carnet)
 );
 
@@ -66,8 +68,10 @@ cargo ENUM('Catedra', 'Planta') not null,
 facultad ENUM('Ingeniería', 'Ciencias Administrativas', 'Ciencias Agropecuarias', 'Ciencias del Deporte', 'Educación', 'Ciencias de la Salud', 'Ciencias Sociales') not null,
 sede ENUM('Fusagasugá', 'Facatativá', 'Girardot', 'Soacha', 'Ubaté', 'Zipaquirá', 'Chía', 'Bogotá') not null,
 id_persona_docente varchar(15) not null,
+unique key unique_id_persona_docente(id_persona_docente),
 foreign key (id_persona_docente) references personal_universidad(id_persona),
 id_rfid_nfc_docente varchar(50) not null,
+unique key unique_id_rfid_nfc_docente(id_rfid_nfc_docente),
 foreign key (id_rfid_nfc_docente) references rfid_nfc(id_carnet)
 );
 
@@ -75,26 +79,30 @@ create table if not exists administrativo (
 cargo varchar(45) not null,
 sede ENUM('Fusagasugá', 'Facatativá', 'Girardot', 'Soacha', 'Ubaté', 'Zipaquirá', 'Chía', 'Bogotá') not null,
 id_persona_administrativo varchar(15) not null,
+unique key unique_id_persona_administrativo(id_persona_administrativo),
 foreign key (id_persona_administrativo) references personal_universidad(id_persona),
 id_rfid_nfc_administrativo varchar(50) not null,
+unique key unique_id_rfid_nfc_administrativo(id_rfid_nfc_administrativo),
 foreign key (id_rfid_nfc_administrativo) references rfid_nfc(id_carnet)
 );
 
 create table if not exists egresado (
 facultad ENUM('Ingeniería', 'Ciencias Administrativas', 'Ciencias Agropecuarias', 'Ciencias del Deporte', 'Educación', 'Ciencias de la Salud', 'Ciencias Sociales') not null,
-carrera ENUM('Admin Empresas', 'Contaduría Pública', 'Gesstión Turística', 'Agrónoma', 'Ambiental', 'Zootecnia', 'Educación Física', 'Ciencias Sociales', 'Electrónica', 'Industrial', 'Sistemas y Computación', 'Sistemas', 'Software', 'Enfermería', 'Música', 'Psicología') not null,
+carrera ENUM('Admin Empresas', 'Contaduría Pública', 'Gestión Turística', 'Agrónoma', 'Ambiental', 'Zootecnia', 'Educación Física', 'Ciencias Sociales', 'Electrónica', 'Industrial', 'Sistemas y Computación', 'Sistemas', 'Software', 'Enfermería', 'Música', 'Psicología') not null,
 ano_prom varchar(8) not null,
 id_persona_egresado varchar(15) not null,
+unique key unique_id_persona_egresado(id_persona_egresado),
 foreign key (id_persona_egresado) references personal_universidad(id_persona),
 id_rfid_nfc_egresado varchar(50) not null,
+unique key unique_id_rfid_nfc_egresado(id_rfid_nfc_egresado),
 foreign key (id_rfid_nfc_egresado) references rfid_nfc(id_carnet)
 );
 
 create table if not exists prestamos (
 id_prestamo int auto_increment primary key,
 nombre_objeto varchar(50) not null,
-inicio_prestamo datetime not null,
-final_prestamo datetime not null,
+inicio_prestamo datetime,
+final_prestamo datetime,
 origen ENUM('Bienestar', 'Biblioteca', 'Sala de Computo') not null,
 id_persona varchar(15) not null,
 foreign key (id_persona) references personal_universidad(id_persona),
--- consultas.sql
+++ consultas.sql
@@ -1,12 +1,17 @@
 SELECT * FROM carnet.estudiante;
 insert into carnet.rfid_nfc values('12345678901357908642');
+INSERT INTO carnet.personal_universidad VALUES ('1007668999', 'Cedula Ciudadania', '13579', '461220188', 'Martha', '', 'Carreño', '', 'mCarreño@hotmail.com', '312349099');
 insert into carnet.estudiante values('Ciencias Agropecuarias', 'Ambiental', 'Facatativá', 8, '1774009874', '12345678901357908642');
-INSERT INTO carnet.personal_universidad VALUES ('1003456946', 'Cedula Ciudadania', '12345', '461220136', 'Cristhian', 'Fernando', 'Monetejo', 'Martínez', 'crismontejo@ucundinamarca.edu.co', '312349075');
-insert into carnet.rfid_nfc values('0987654321357908642');
+insert into carnet.docentes values ('Encargo', 'Planta', 'Ciencias del Deporte', 'Soacha', '1003456946', '12345');
+insert into carnet.administrativo values ('Secretaria Sistemas', 'Facatativá', '1007668827', '67890');
+insert into carnet.egresado values ('Ingeniería', 'Sistemas y Computación', '2019-1', '1007668999', '13579');
+insert into carnet.prestamos (nombre_objeto, inicio_prestamo, origen, id_persona, id_rfid_nfc) values ('Balón voleibol', '2022-04-01 20:27:40', 'Bienestar', '1007668974', '0987654321357908642');
+insert into carnet.sala_computo values ('2022-04-01 20:27:40', '2022-04-01 20:27:40', '201', '3958398583975', '1003456946', '12345');
+insert into carnet.laboratorio values ('2022-04-01 20:27:40', '2022-04-01 20:27:40', '1007668974', '0987654321357908642');
+insert into carnet.autodiag (fiebre, fatiga, cabeza, tos, malestar, musculos, contacto_covid19, respiracion, pecho, nariz, garganta, olores, diarrea, temperatura, ingresar_elementos, id_persona, id_rfid_nfc) values (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 36.7, 0, '1007668974', '0987654321357908642');
+SHOW INDEXES FROM carnet.estudiante;
 select id_persona, codigo, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, sede, facultad, sede, semestre, email, celular from carnet.personal_universidad, carnet.estudiante where personal_universidad.id_persona = estudiante.id_persona_estudiante;
 SELECT codigo, carrera, primer_apellido, semestre FROM carnet.personal_universidad JOIN estudiante ON personal_universidad.id_persona = estudiante.id_persona_estudiante;
 select codigo, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, ingreso, salida from carnet.personal_universidad join laboratorio on personal_universidad.id_persona = laboratorio.id_persona;
 SELECT * FROM carnet.personal_universidad JOIN estudiante USING(id_persona);/*para usar 'using' deben tener el mismo nombre de variables*/
 select codigo, primer_nombre, primer_apellido, ingreso, salida from carnet.personal_universidad join laboratorio on personal_universidad.id_persona = laboratorio.id_persona;
-
-
